9b764f454e5d527138cad6bcc79021ff8148725b
Roberlande Silva <roberlanderrsilva@gmail.com>
2022-10-29 21:24:35 -0400

    subindo arquivos do curso sobre basico Docker
    concluindo up load

---
+++ banco.sql
@@ -0,0 +1,10 @@
+CREATE TABLE dados (
+    AlunoID int,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Telefone varchar(15),
+    Email varchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),
+    Host varchar(50)
+);
